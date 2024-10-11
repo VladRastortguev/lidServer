c135aab7f1bc766cb91f561023446f5344ef1e88
VladRastortguev <vladrastorguev.ru@gmail.com>
2024-10-11 16:41:18 +0600

    fix --origin 5

--- lidapi.js
+++ lidapi.js
@@ -1,41 +1,28 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const router = require('./router');
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const cookiePareser = require('cookie-parser')
+const router = require('./router')
 
-const PORT = process.env.PORT || 5001;
-const app = express();
+const PORT = process.env.PORT || 5001
+const app = express()
 
-// Настройка CORS
-const allowedOrigins = ['https://www.kia-bishkek.kg/'];
+app.use(express.json())
+app.use(cookiePareser())
 app.use(cors({
     credentials: true,
-    origin: function (origin, callback) {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    }
-}));
+}))
+app.options('*', cors()); 
+app.use('/lidapi', router)
 
-// Middleware
-app.use(express.json());
-app.use(cookieParser());
-
-// Маршруты
-app.use('/lidapi', router);
-
-// Запуск сервера
 const start = async () => {
-    try {
-        app.listen(PORT, () => {
-            console.log(`Server started on PORT = ${PORT}`);
-        });
+    try { 
+        app.listen(5001, () => {
+            console.log(`Server started on PORT = ${PORT}`);            
+        })
     } catch (e) {
-        console.error('Error starting server:', e);
+        console.log(e);        
     }
-};
+}
 
-start();
+start()
