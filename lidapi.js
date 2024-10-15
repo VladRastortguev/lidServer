cce6fc31dd13212729cf90dcb7bb2fc2e252398a
VladRastortguev <vladrastorguev.ru@gmail.com>
2024-10-15 12:04:36 +0600

    fix -m headers 3

--- lidapi.js
+++ lidapi.js
@@ -9,16 +9,18 @@ const app = express()
 
 app.use(express.json())
 app.use(cookiePareser())
-// app.use(cors({
-//     credentials: true,
-// }))
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    next()
-})
+app.use(cors({
+    credentials: true,
+    origin: '*',
+    methods: 'GET, POST'
+}))
+// app.use(function(req, res, next) {
+//     res.setHeader('Access-Control-Allow-Origin', '*')
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+//     res.setHeader('Access-Control-Allow-Credentials', true)
+//     next()
+// })
 app.use('/lidapi', router)
 
 
