0193a7ab93b9e2c73a8c795ecf7ea2e19b61e389
VladRastortguev <vladrastorguev.ru@gmail.com>
2024-10-15 15:40:18 +0600

    fix --import axios

--- service/kia-service.js
+++ service/kia-service.js
@@ -1,9 +1,9 @@
-const { default: axios } = require("axios")
+const { axios } = require("axios")
 
 class kiaServcie {
     async setKiaNewKar(obj) {
         try {
-            const setLid = await axios.post(`${process.env.DB_API}`, obj, {
+            const setLid = await axios.post(`http://192.168.2.26:35421/MK_test_AA6kg_Vlad_dataCopy2/hs/att/create_lead`, obj, {
                 auth: {
                     username: 'hs',
                     password: 'w9aX'
